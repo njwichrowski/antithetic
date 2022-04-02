8f353ab4abbcdc580329d4aca2dfc2e9dd354773
Noah J. Wichrowski <53314923+njwichrowski@users.noreply.github.com>
2022-04-02 18:58:04 -0400

    Update __init__.py
    Add more details to package docstring.

--- antithetic/__init__.py
+++ antithetic/__init__.py
@@ -1,19 +1,31 @@
 """
 Antithetic Random Variables
 
-This module provides a wrapper for the ``numpy.random.Generator`` class for
+This package provides a wrapper for the ``numpy.random.Generator`` class for
 generating correlated sequences of random variables. Such sequences contain
 pairs of values with correlation value determined by a user-specified param-
-eter, though values from different pairs are independent.
+eter, though values from different pairs are independent. For theoretical
+reference, see Chapter 9.3 of [2].
 
-For theoretical reference, see Chapter 9.3 of:
-    Kroese, D. P.; Taimre, T.; Botev, Z. I. (2011). Handbook of Monte Carlo
-    methods. John Wiley & Sons.
+A multivariate normal distribution has Gaussian marginal distributions, so it
+is simple to generate correlated pairs of normal random variables. A greater
+challenge, however, is generating random variables from other distributions
+that are identically distributed but have a specified correlation. Carlo and
+Nelson [1] describe a framework for generating the desired distribution from
+appropriately correlated normal random variables.
     
 Dependencies
 ------------
 numpy
 scipy
+
+References
+----------
+[1] Cario, Marne C.; Nelson, Barry L. ``Modeling and Generating Random Vectors
+    with Arbitrary Marginal Distributions and Correlation Matrix,'' (1997).
+    DOI: 10.1145/937332.937336
+[2] Kroese, D. P.; Taimre, T.; Botev, Z. I. (2011). Handbook of Monte Carlo
+    Methods. John Wiley & Sons.
 
 """
 from numpy import array as np_array, sqrt as np_sqrt
