5b1bd60d8a3b31eaea57292637a03fe12475bdad
njwichrowski <njwichrowski@gmail.com>
2022-04-30 15:31:09 -0400

    Place Uniform under new InverseCDF subclass.

--- antithetic/scalars.py
+++ antithetic/scalars.py
@@ -103,7 +103,117 @@ class Normal(AntitheticScalar):
         sequence = self.get_sequence_raw_normal(N, method, mix_singles)
         return self.scale*sequence + self.loc
 
-class Uniform(AntitheticScalar):
+class InverseCDF(AntitheticScalar):
+    """
+    Common or antithetic random variables with marginal distribution specified
+    via cumulative distribution function (CDF). This subclass generates
+    correlated uniform random variables and applies the inverse CDF to obtain
+    a desired marginal distribution.
+    
+    The uniform variables are derived from the raw normal random variables of
+    the AntitheticScalar base class. Cario and Nelson [1] report an analytical
+    relationship between the correlation of standard normal random variables
+    and the uniform variables that result from transforming by inverse normal
+    CDF, which we use here.
+    
+    Additional subclasses provide common distributions.
+    """
+    
+    def __init__(self, correlation, func, seed = None, param_names = None):
+        """
+        Common or antithetic random variables generated from the standard
+        uniform distribution by applying an inverse CDF.
+
+        Parameters
+        ----------
+        correlation : float
+            Correlation of the *underlying uniform* random values to be gener-
+            ated. Must be in the range -1 <= correlation <= +1. In general,
+            the resulting nonuniform distribution will have a different value
+            of intra-pair correlation, and the lower bound on achievabe output
+            correlations may not be -1.
+        
+        func : callable
+            A function specifying how to transform the underlying uniform ran-
+            dom variables, i.e., the inverse CDF of the desired distribution.
+            
+            This function should be vectorized over a single positional
+            argument, corresponding to pre-transformed values, and be able to
+            accept (while possibly ignoring) arbitrary keyword arguments,
+            including any relevant distributional parameters.
+        
+        seed : {int, array_like[ints], SeedSequence}, optional
+            Random seed used to initialize the pseudo-random number generator.
+            Passed directly to the numpy.random.default_rng() method. If None
+            (default) is provided, a seed will be automatically generated.
+        
+        param_names : list of str, optional
+            A list of the names for distributional parameters defined at the
+            subclass level. When setting an attribute with a name in this list,
+            the internal flag indicating whether the next value has already
+            been generated is forced to False, in order to prevent such a value
+            remaining in the stream after altering the generator distribution.
+        """
+        self.transformation = func
+        raw_correlation = 2.0*np.sin(np.pi*correlation/6.0)
+        super().__init__(raw_correlation, seed, param_names)
+    
+    def get_next(self):
+        """
+        Generate the next correlated random scalar.
+        """
+        # Get next raw normal and convert to uniform:
+        raw_normal_value = self.get_next_raw_normal()
+        base_uniform_value = norm.cdf(raw_normal_value)
+        
+        # Apply inverse CDF and return result:
+        return self.transformation(
+            base_uniform_value,
+            **self.distributional_parameters
+        )
+    
+    def get_sequence(self, N, method = "zip", mix_singles = True):
+        """
+        Generate multiple subsequent values in a sequence of correlated
+        uniform random scalars, as if calling get_next_raw_normal the same
+        number of times and collecting the results. Optionally rearrange the
+        values before returning, so that paired values are not adjacent.
+        
+        Parameters
+        ----------
+        N : int
+            The number of values to generate.
+        
+        method : {"zip", "shuffle", "concatenate"}, optional
+            Procedure for assembling the overall sequence from two paired
+            subsequences with correlated entries.  (Default: "zip")
+                "zip" : Combine subsequences entry-by-entry, so that each item
+                        is adjacent to the other member of its pair.
+                "shuffle" : Randomly permute all items.
+                "concatenate" : Combine the subsequences end-to-end.
+        
+        mix_singles : bool, optional
+            If True (default) and method == "shuffle", include unpaired values
+            on the beginning or end of the sequence, if present, in the
+            shuffling procedure. If False, such values will be kept in-place
+            while shuffling the interior pairs entirely contained in sequence.
+        
+        Returns
+        -------
+        seq : np.ndarray (N,)
+            The sequence of correlated, marginally uniform scalars.
+        """
+        # Get subsequent raw normals and convert to uniform:
+        normal_values = self.get_sequence_raw_normal(N, method, mix_singles)
+        base_uniform_values = norm.cdf(normal_values)
+        
+        # Apply inverse CDF and return results:
+        return self.transformation(
+            base_uniform_values,
+            **self.distributional_parameters
+        )
+
+class Uniform(InverseCDF):
     """
     Common or antithetic uniform random variables.
     """
@@ -140,53 +250,16 @@ class Uniform(AntitheticScalar):
         self.low = min(low, high)
         self.high = max(low, high)
         
-        raw_correlation = 2.0*np.sin(np.pi*correlation/6.0)
-        super().__init__(raw_correlation, seed, param_names = ["low", "high"])
-    
-    def get_next(self):
-        """
-        Generate the next correlated uniform random scalar.
-        """
-        raw_normal_value = self.get_next_raw_normal()
-        base_uniform_value = norm.cdf(raw_normal_value)
-        return self.range*base_uniform_value + self.low
-    
-    def get_sequence(self, N, method = "zip", mix_singles = True):
-        """
-        Generate multiple subsequent values in a sequence of correlated
-        uniform random scalars, as if calling get_next_raw_normal the same
-        number of times and collecting the results. Optionally rearrange the
-        values before returning, so that paired values are not adjacent.
-        
-        Parameters
-        ----------
-        N : int
-            The number of values to generate.
-        method : {"zip", "shuffle", "concatenate"}, optional
-            Procedure for assembling the overall sequence from two paired
-            subsequences with correlated entries.  (Default: "zip")
-                "zip" : Combine subsequences entry-by-entry, so that each item
-                        is adjacent to the other member of its pair.
-                "shuffle" : Randomly permute all items.
-                "concatenate" : Combine the subsequences end-to-end.
-        mix_singles : bool, optional
-            If True (default) and method == "shuffle", include unpaired values
-            on the beginning or end of the sequence, if present, in the
-            shuffling procedure. If False, such values will be kept in-place
-            while shuffling the interior pairs entirely contained in sequence.
-        
-        Returns
-        -------
-        seq : np.ndarray (N,)
-            The sequence of correlated, marginally uniform scalars.
-        """
-        normal_values = self.get_sequence_raw_normal(N, method, mix_singles)
-        base_uniform_values = norm.cdf(normal_values)
-        return self.range*base_uniform_values + self.low
+        super().__init__(
+            correlation = correlation,
+            func = Uniform.inverse_CDF,
+            seed = seed,
+            param_names = ["low", "high"]
+        )
     
     def change_correlation(self, new_correlation):
         """
-        Specify a new value for the within-pair correlation of the marginally
+        Specify a new value for the intra-pair correlation of the marginally
         uniform variables to generate. Computes the required raw_correlation
         for the underlying normal variables and updates accordingly.
         
@@ -199,6 +272,8 @@ class Uniform(AntitheticScalar):
         if not(-1.0 <= new_correlation <= 1.0):
             raise ValueError("Invalid correlation: %r" % (new_correlation,))
         
+        # Update raw correlation. The setter method defined in base class
+        # AntitheticScalar handles resetting the generation flags:
         self.raw_correlation = 2.0*np.sin(np.pi*new_correlation/6.0)
     
     @property
@@ -220,3 +295,7 @@ class Uniform(AntitheticScalar):
     @property
     def correlation(self):
         return 6.0*np.arcsin(0.5*self.raw_correlation)/np.pi
+    
+    @classmethod
+    def inverse_CDF(cls, u, low = 0.0, high = 1.0, **kwargs):
+        return low + (high - low)*u
